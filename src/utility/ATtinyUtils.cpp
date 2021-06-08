4fb176da0fac7d62e134bf383194a78e9b094c0a
Armin <armin.arduino@gmail.com>
2021-06-08 21:56:05 +0200

    Updated 3. Party libraries

--- src/utility/ATtinyUtils.cpp
+++ src/utility/ATtinyUtils.cpp
@@ -20,7 +20,7 @@
  *  along with this program.  If not, see <http://www.gnu.org/licenses/gpl.html>.
  *
  */
-#if defined(__AVR_ATtiny25__) || defined(__AVR_ATtiny45__) || defined(__AVR_ATtiny85__) || defined(__AVR_ATtiny87__) || defined(__AVR_ATtiny167__)
+#if defined(__AVR__) && defined(__AVR_ATtiny25__) || defined(__AVR_ATtiny45__) || defined(__AVR_ATtiny85__) || defined(__AVR_ATtiny87__) || defined(__AVR_ATtiny167__)
 
 //#define DEBUG
 #ifdef DEBUG
@@ -173,9 +173,12 @@ void changeDigisparkClock() {
         writeUnsignedByteHex(tOSCCAL);
         writeString(F(" to "));
         writeUnsignedByteHex(tStoredOSCCAL);
-        writeCRLF();#
+        writeCRLF();
 #endif
     }
 }
-
+#elif defined(ARDUINO_ARCH_APOLLO3)
+    void AttinyUtilsDummyToAvoidBFDAssertions(){
+        ;
+    }
 #endif //  defined(__AVR_ATtiny85__)
